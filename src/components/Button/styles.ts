578de0cf8912eb676fdf81e765b3eb2c6cd3f2ce
hozenyth.andrade <hozenyth.andrade@gmail.com>
2024-10-29 16:09:38 -0300

    Feito botão ser desabilitado quando formulario nao está preenchido corretamente.

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
 
-
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<{ disabled?: boolean }>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
-    color: #FFF;
-
+    background-color: ${({ disabled }) => (disabled ? '#ccc' : '#81259D')}; /* Cor de fundo */
+    color: ${({ disabled }) => (disabled ? '#666' : '#FFF')}; /* Cor do texto */
+    
     border: 1px solid #81259D;
     border-radius: 21px;
 
     &:hover {
-        opacity: 0.6;
-        cursor:pointer;
+        opacity: ${({ disabled }) => (disabled ? '1' : '0.6')}; /* Evita alteração de opacidade quando desabilitado */
+        cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')}; /* Muda o cursor */
     }
-`
+
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')}; /* Impede interações quando desabilitado */
+`;
